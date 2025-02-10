8ccfeb9704a866e2ec627dfb51022b7d4cc0b454
Jonathan Xina <john.xina.p88@gmail.com>
2025-02-10 14:53:31 +0700

    feat: refactor

--- src/websocket/subscriptions.ts
+++ src/websocket/subscriptions.ts
@@ -1,142 +1,124 @@
 import { MarkPrice, OrderBook } from "../types";
 import { WebSocketClient } from "./connection";
 
+type SubscriptionType = "markPricesV2" | "l2BookV2";
+type MessageHandler = (message: any) => void;
+
+interface Subscription {
+  type: SubscriptionType;
+  symbol?: string;
+}
+
+interface SubscriptionCallback {
+  callback: Function;
+  messageHandler: MessageHandler;
+}
+
 export class WebSocketSubscriptions {
   private ws: WebSocketClient;
-  private activeSubscriptions: Map<string, Set<Function>> = new Map();
+  private subscriptions: Map<string, Set<SubscriptionCallback>> = new Map();
 
   constructor(ws: WebSocketClient) {
     this.ws = ws;
   }
 
-  private addSubscriptionCallback(key: string, callback: Function): void {
-    if (!this.activeSubscriptions.has(key)) {
-      this.activeSubscriptions.set(key, new Set());
-    }
-    this.activeSubscriptions.get(key)?.add(callback);
+  private getSubscriptionKey(subscription: Subscription): string {
+    return JSON.stringify(subscription);
   }
 
-  private async subscribe(subscription: {
-    type: string;
-    [key: string]: any;
-  }): Promise<void> {
+  private addSubscription(
+    subscription: Subscription,
+    callback: Function,
+    messageHandler: MessageHandler
+  ): void {
+    const key = this.getSubscriptionKey(subscription);
+    if (!this.subscriptions.has(key)) {
+      this.subscriptions.set(key, new Set());
+    }
+    this.subscriptions.get(key)?.add({ callback, messageHandler });
+  }
+
+  private async subscribe(subscription: Subscription): Promise<void> {
     await this.ws.sendMessage({
       method: "subscribe",
-      subscription: subscription,
+      subscription,
     });
   }
 
-  private async unsubscribe(subscription: {
-    type: string;
-    [key: string]: any;
-  }): Promise<void> {
+  private async unsubscribe(subscription: Subscription): Promise<void> {
     await this.ws.sendMessage({
       method: "unsubscribe",
-      subscription: subscription,
+      subscription,
     });
   }
 
-  async subscribeToMarkPrices(callback: (data: any) => void): Promise<void> {
-    if (typeof callback !== "function") {
-      throw new Error("Callback must be a function");
-    }
-
-    const subscriptionType = { type: "markPricesV2" };
-    const subscriptionKey = JSON.stringify(subscriptionType);
-
-    // Remove existing subscription if any
-    if (this.activeSubscriptions.has(subscriptionKey)) {
-      await this.unsubscribeFromMarkPrices();
-    }
-
-    this.addSubscriptionCallback(subscriptionKey, callback);
-
-    const messageHandler = async (message: any) => {
-      if (message.type === "markPricesV2") {
-        if (message.data) {
-          const response = message.data.map((each: any) => {
-            return {
-              symbol: each.symbol,
-              markPrice: each.mark_price,
-              indexPrice: each.index_price,
-            } as MarkPrice;
-          });
-          callback(response);
-        }
-      }
-    };
-
-    (callback as any).__messageHandler = messageHandler;
-    this.ws.on("message", messageHandler);
-    await this.subscribe(subscriptionType);
-  }
-
-  async unsubscribeFromMarkPrices(): Promise<void> {
-    const subscriptionType = { type: "markPricesV2" };
-    const subscriptionKey = JSON.stringify(subscriptionType);
-    const callbacks = this.activeSubscriptions.get(subscriptionKey);
+  private removeSubscription(subscription: Subscription): void {
+    const key = this.getSubscriptionKey(subscription);
+    const callbacks = this.subscriptions.get(key);
 
     if (callbacks) {
-      for (const callback of callbacks) {
-        const messageHandler = (callback as any).__messageHandler;
-        if (messageHandler) {
-          this.ws.removeListener("message", messageHandler);
-          delete (callback as any).__messageHandler;
-        }
+      for (const { messageHandler } of callbacks) {
+        this.ws.removeListener("message", messageHandler);
       }
-      this.activeSubscriptions.delete(subscriptionKey);
+      this.subscriptions.delete(key);
     }
-
-    await this.unsubscribe(subscriptionType);
   }
 
-  async subscribeToOrderbook(
-    symbol: string,
-    callback: (data: any) => void
+  async subscribeToMarkPrices(
+    callback: (data: MarkPrice[]) => void
   ): Promise<void> {
     if (typeof callback !== "function") {
       throw new Error("Callback must be a function");
     }
 
-    const subscriptionType = { type: "l2BookV2", symbol: symbol };
-    const subscriptionKey = JSON.stringify(subscriptionType);
+    const subscription: Subscription = { type: "markPricesV2" };
 
-    // Remove existing subscription if any
-    if (this.activeSubscriptions.has(subscriptionKey)) {
-      await this.unsubscribeFromOrderbook(symbol);
-    }
-
-    this.addSubscriptionCallback(subscriptionKey, callback);
-
-    const messageHandler = async (message: any) => {
-      if (message.type === "l2BookV2") {
-        if (message.data) {
-          callback(message.data as OrderBook);
-        }
+    const messageHandler: MessageHandler = (message: any) => {
+      if (message.type === "markPricesV2" && message.data) {
+        const markPrices = message.data.map((item: any) => ({
+          symbol: item.symbol,
+          markPrice: item.mark_price,
+          indexPrice: item.index_price,
+        }));
+        callback(markPrices);
       }
     };
 
-    (callback as any).__messageHandler = messageHandler;
+    this.addSubscription(subscription, callback, messageHandler);
     this.ws.on("message", messageHandler);
-    await this.subscribe(subscriptionType);
+    await this.subscribe(subscription);
+  }
+
+  async unsubscribeFromMarkPrices(): Promise<void> {
+    const subscription: Subscription = { type: "markPricesV2" };
+    this.removeSubscription(subscription);
+    await this.unsubscribe(subscription);
+  }
+
+  async subscribeToOrderbook(
+    symbol: string,
+    callback: (data: OrderBook) => void
+  ): Promise<void> {
+    if (typeof callback !== "function") {
+      throw new Error("Callback must be a function");
+    }
+
+    const subscription: Subscription = { type: "l2BookV2", symbol };
+
+    const messageHandler: MessageHandler = (message: any) => {
+      if (message.type === "l2BookV2" && message.data) {
+        callback(message.data as OrderBook);
+      }
+    };
+
+    this.addSubscription(subscription, callback, messageHandler);
+    this.ws.on("message", messageHandler);
+    await this.subscribe(subscription);
   }
 
   async unsubscribeFromOrderbook(symbol: string): Promise<void> {
-    const subscriptionType = { type: "l2BookV2", symbol: symbol };
-    const subscriptionKey = JSON.stringify(subscriptionType);
-    const callbacks = this.activeSubscriptions.get(subscriptionKey);
-
-    if (callbacks) {
-      for (const callback of callbacks) {
-        const messageHandler = (callback as any).__messageHandler;
-        if (messageHandler) {
-          this.ws.removeListener("message", messageHandler);
-          delete (callback as any).__messageHandler;
-        }
-      }
-      this.activeSubscriptions.delete(subscriptionKey);
-    }
-
-    await this.unsubscribe(subscriptionType);
+    const subscription: Subscription = { type: "l2BookV2", symbol };
+    this.removeSubscription(subscription);
+    await this.unsubscribe(subscription);
   }
 }
